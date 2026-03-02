2b0597e6a9344a948b70ddbea059ce3b3c177888
Sachintha Prabashana <sachinthaprabhashana2003@gmail.com>
2026-03-02 19:34:08 +0530

    Feature: Initialized Crop Inventory Service with PostgreSQL

---
+++ domain-services/crop-inventory-service/src/main/java/lk/ijse/cropinventoryservice/controller/CropManageController.java
@@ -0,0 +1,14 @@
+package lk.ijse.cropinventoryservice.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/crops")
+public class CropManageController {
+    public ResponseEntity<String> checkCropManagement(){
+        return new ResponseEntity<>("OK", HttpStatus.OK);
+    }
+}
---
+++ domain-services/crop-inventory-service/src/main/java/lk/ijse/cropinventoryservice/service/CropService.java
@@ -0,0 +1,4 @@
+package lk.ijse.cropinventoryservice.service;
+
+public interface CropService {
+}
