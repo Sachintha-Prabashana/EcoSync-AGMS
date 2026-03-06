87861dec1afae576cd8ad05d7d1ac1bc10721970
Sachintha Prabashana <sachinthaprabhashana2003@gmail.com>
2026-03-06 22:31:44 +0530

    Add Centralized Cloud Config Server implementation

--- domain-services/crop-inventory-service/src/main/java/lk/ijse/cropinventoryservice/controller/CropManageController.java
+++ domain-services/crop-inventory-service/src/main/java/lk/ijse/cropinventoryservice/controller/CropManageController.java
@@ -2,12 +2,14 @@ package lk.ijse.cropinventoryservice.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/crops")
 public class CropManageController {
+    @GetMapping
     public ResponseEntity<String> checkCropManagement(){
         return new ResponseEntity<>("OK", HttpStatus.OK);
     }
---
+++ infrastructure-services/api-gateway/src/main/java/lk/ijse/apigateway/SecurityConfig.java
@@ -0,0 +1,22 @@
+package lk.ijse.apigateway;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        return http
+                .csrf(csrf -> csrf.disable()) // desable CSRF protection for simplicity
+                .authorizeExchange(exchanges -> exchanges
+                        .anyExchange().permitAll() // allow all requests without authentication
+                )
+                .build();
+    }
+}
